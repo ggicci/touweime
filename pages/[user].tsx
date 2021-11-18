a356620a1e395c56621dae4ec58d866db99266e2
Ggicci <ggicci.t@gmail.com>
2021-11-18 19:30:33 +0800

    feat: use settings

--- pages/[user].tsx
+++ pages/[user].tsx
@@ -1,14 +1,21 @@
-import { Container, Stack, Typography } from '@mui/material'
+import { Container, Paper, Stack, Typography } from '@mui/material'
 import { useRouter } from 'next/router'
+import { useSettings } from 'sdk/settings'
 
 const UserHome = () => {
   const router = useRouter()
   const { user: username } = router.query
+  const { data: settings } = useSettings()
+
+  if (!settings) {
+    return null
+  }
 
   return (
-    <Container>
+    <Container component={Paper}>
       <Stack>
         <Typography>{username}</Typography>
+        <pre>{JSON.stringify(settings, null, 2)}</pre>
       </Stack>
     </Container>
   )
