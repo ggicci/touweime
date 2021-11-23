a7193d468f11b585fdc6a08b4d136ebf5f3f55c8
Ggicci <ggicci.t@gmail.com>
2021-11-23 19:08:07 +0800

    feat: add qrcode

--- components/User/AboutMe.tsx
+++ components/User/AboutMe.tsx
@@ -3,6 +3,7 @@ import Paper from '@mui/material/Paper'
 import Typography from '@mui/material/Typography'
 import DOMPurify from 'dompurify'
 import { marked } from 'marked'
+import useTranslation from 'next-translate/useTranslation'
 import React from 'react'
 import { Settings } from 'sdk/settings'
 
@@ -11,12 +12,21 @@ interface AboutMeProps {
 }
 
 const AboutMe = (props: AboutMeProps) => {
+  const { t } = useTranslation('common')
   const { settings } = props
+
+  let aboutMeContent = null
   const htmlAboutMe = DOMPurify.sanitize(marked.parse(settings.about_me))
+
+  if (htmlAboutMe === '') {
+    aboutMeContent = <Typography textAlign="center">{t('emptyAboutMe')}</Typography>
+  } else {
+    aboutMeContent = <Typography dangerouslySetInnerHTML={{ __html: htmlAboutMe }}></Typography>
+  }
 
   return (
     <Container component={Paper} variant="outlined" sx={{ py: 2 }}>
-      <Typography dangerouslySetInnerHTML={{ __html: htmlAboutMe }}></Typography>
+      {aboutMeContent}
     </Container>
   )
 }
