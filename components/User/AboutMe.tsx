71908e40dc2ea0768939d5b951b4353ccab49c07
Ggicci <ggicci.t@gmail.com>
2021-12-20 11:42:11 +0800

    feat: empty bio

--- components/User/AboutMe.tsx
+++ components/User/AboutMe.tsx
@@ -1,5 +1,6 @@
 import Container from '@mui/material/Container'
 import Paper from '@mui/material/Paper'
+import Typography from '@mui/material/Typography'
 import MarkdownRenderer from 'components/MarkdownRenderer'
 import { Settings } from 'lib/settings'
 import useTranslation from 'next-translate/useTranslation'
@@ -13,9 +14,16 @@ const AboutMe = (props: AboutMeProps) => {
   const { t } = useTranslation('common')
   const { settings } = props
 
+  let content
+  if (settings.about_me) {
+    content = <MarkdownRenderer markdown={settings.about_me}></MarkdownRenderer>
+  } else {
+    content = <Typography>{t('user-page.about-me.empty-bio')}</Typography>
+  }
+
   return (
     <Container component={Paper} variant="outlined" sx={{ py: 2 }}>
-      <MarkdownRenderer markdown={settings.about_me}></MarkdownRenderer>
+      {content}
     </Container>
   )
 }
